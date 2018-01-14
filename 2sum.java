cc11fd4495f8565a3347927cb2e4a16dc66b690a
KekeZy <33374748+KekeZy@users.noreply.github.com>
2018-01-14 20:59:34 +0800

    Update 2sum.java

--- 2sum.java
+++ 2sum.java
@@ -1,1 +1,22 @@
-111
+问题描述：
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+显然暴力搜索两层循环可以解决，下面是使用哈希表对算法进行优化，时间复杂度O(n). (没有考虑数据异常的情况，比如nums=null)
+
+ class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer> map = new HashMap<Integer,Integer>(); 
+        int[] res = new int[2];
+        for(int i=0;i<nums.length;i++){
+              if(map.containsKey(target-nums[i])){
+                  res[0]=map.get(target-nums[i]);
+                  res[1]=i;
+                  return res;
+              }
+              map.put(nums[i],i);
+        }
+        return null;
+    }
+}
+
